5316a111eb5c16154e9174fe6416f04ca53b1540
YukiYuigishi <me@yuigishi.dev>
2022-03-21 21:19:11 +0900

    delete

--- src/twitterSearcher.ts
+++
@@ -1,6 +0,0 @@
-import Twitter from "twitter";
-
-const TwitterSearcher = (bearerToken: string, searchQuery: string) => {
-  console.log(searchQuery, bearerToken);
-};
-export default TwitterSearcher;
